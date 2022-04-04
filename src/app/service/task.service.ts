4a9a0d1daf82b20622001430f78934d43db9ae82
Franco <francoljoka2@gmail.com>
2022-04-04 18:41:58 -0300

    Servicio y servidor de tareas listo

---
+++ src/app/service/task.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { Task } from 'src/app/Task';
+import { TASKS } from '../mock-tasks';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TaskService {
+  private apiUrl = 'http://localhost:5000/task';
+
+  constructor(private http: HttpClient) {}
+
+  getTask(): Observable<Task[]> {
+    return this.http.get<Task[]>(this.apiUrl);
+  }
+}
