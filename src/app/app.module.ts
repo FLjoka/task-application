f60f066a978ffbc885c62a05f1a107e30f739dc9
Franco <francoljoka2@gmail.com>
2022-04-03 21:05:35 -0300

    Lista de items

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
 import { ButtonComponent } from './component/button/button.component';
+import { TasksComponent } from './component/tasks/tasks.component';
+import { TasksItemsComponent } from './component/tasks-items/tasks-items.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ButtonComponent
+    ButtonComponent,
+    TasksComponent,
+    TasksItemsComponent
   ],
   imports: [
     BrowserModule
